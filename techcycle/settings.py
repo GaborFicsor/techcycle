90d13023a8733bc44c45ae9fd98c575a04920779
GaborFicsor <ficsorgabor@gmail.com>
2023-05-31 21:50:52 +0000

    Add 'localhost' to ALLOWED_HOSTS in settings.py, so that the app can run in the local environment

--- techcycle/settings.py
+++ techcycle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j)^v4)e#3-j#5s9fzmeek83n-5xe61d(u#_d6tlfvz$upe39zu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','8000-gaborficsor-techcycle-wgmppxamqda.ws-eu98.gitpod.io']
 
 
 # Application definition
