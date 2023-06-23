198468e0b5be01c6eb95aa5e0293d635a8a1d375
GaborFicsor <ficsorgabor@gmail.com>
2023-06-23 19:38:46 +0000

    Create phone model and Load data from phone.json

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Laptop
+from .models import Category, Laptop, Phone
 
 
 
 admin.site.register(Category)
 admin.site.register(Laptop)
+admin.site.register(Phone)
--- products/models.py
+++ products/models.py
@@ -76,3 +76,34 @@ class Laptop(models.Model):
 
     def __str__(self):
         return f'{self.brand} {self.series} {self.model}'
+
+class Phone(models.Model):
+
+    OS = [
+        ('Android', 'Android'),
+        ('iOS', 'iOS')
+    ]
+    
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    brand = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=8, null=True, blank=True, decimal_places=2)
+    series = models.CharField(max_length=254)
+    model = models.CharField(max_length=254)
+    sim = models.CharField(max_length=254)
+    cpu = models.CharField(max_length=254)
+    ram = models.CharField(max_length=254)
+    storage_size = models.CharField(max_length=254)
+    battery = models.CharField(max_length=254)
+    screen_size = models.DecimalField(max_digits=4, decimal_places=2)
+    resolution = models.CharField(max_length=254)
+    camera_front = models.CharField(max_length=254)
+    camera_rear = models.CharField(max_length=254)
+    os = models.CharField(max_length=254, choices=OS)
+    color = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.brand} {self.series} {self.model}'
