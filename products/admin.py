cf3e4a5a2448b3541a0f18abe3081ea83b328a08
GaborFicsor <ficsorgabor@gmail.com>
2023-06-23 20:08:54 +0000

    Customize admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,64 @@
 from django.contrib import admin
-from .models import Category, Laptop, Phone
+from .models import Category, Laptop, Phone, Smartwatch, Console
 
 
 
-admin.site.register(Category)
-admin.site.register(Laptop)
-admin.site.register(Phone)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class LaptopAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand',
+        'series',
+        'model',
+        'cpu_brand',
+        'cpu_name',
+        'storage_size',
+        'ram',
+        'sale',
+        'price',
+    )
+    
+
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand',
+        'series',
+        'model',
+        'storage_size',
+        'ram',
+        'sale',
+        'price',
+    )
+    
+
+class SmartwatchAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand',
+        'series',
+        'model',
+        'sale',
+        'price',
+    )
+    
+
+class ConsoleAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand',
+        'series',
+        'model',
+        'storage_size',
+        'sale',
+        'price',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Laptop, LaptopAdmin)
+admin.site.register(Phone, PhoneAdmin)
+admin.site.register(Smartwatch, SmartwatchAdmin)
+admin.site.register(Console, ConsoleAdmin)
