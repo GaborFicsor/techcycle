e870e525c385bf3f8289956bb3a35abcbde4ad50
GaborFicsor <ficsorgabor@gmail.com>
2023-06-25 20:49:43 +0000

    Add landing page styling

--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
+import random
 from django.shortcuts import render
+from products.models import Laptop
 
 
 def index(request):
     """A view to return the index page"""
-    return render(request, 'home/index.html')
+
+    laptops = list(Laptop.objects.all())
+
+    laptops = random.sample(laptops, 4)
+
+    context = {
+        'laptops': laptops
+    }
+
+    return render(request, 'home/index.html', context)
