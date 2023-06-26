4b8166c54f2ac4618597731d47e3eba5002af3e6
GaborFicsor <ficsorgabor@gmail.com>
2023-06-26 18:22:54 +0000

    Add FAQ model to database

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import FAQ
 
-# Register your models here.
+
+class FAQAdmin(admin.ModelAdmin):
+    list_display = {
+        'question',
+        'answer',
+    }
+
+admin.site.register(FAQ, FAQAdmin)
