25c43a3783ed474f58e9022d8604f4ed7da3ee26
GaborFicsor <ficsorgabor@gmail.com>
2023-07-03 18:25:46 +0000

    Modify products models

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Category(models.Model):
@@ -6,8 +8,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    friendly_name = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -15,12 +17,44 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
-class Laptop(models.Model):
 
-    CHOICES = [
-        ('yes', 'Yes'),
-        ('no', 'No'),
-    ]
+class Product(models.Model):
+    CHOICES = (
+        ('acceptable', 'Acceptable'),
+        ('good', 'Good'),
+        ('excellent', 'Excellent'),
+    )
+    brand = models.CharField(max_length=50)
+    series = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=8, null=True, blank=True, decimal_places=2)
+    condition = models.CharField(max_length=20, choices=CHOICES, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    stock_acceptable = models.PositiveIntegerField(default=0)
+    stock_good = models.PositiveIntegerField(default=0)
+    stock_excellent = models.PositiveIntegerField(default=0)
+    
+    def save(self, *args, **kwargs):
+        # Check the condition of the product
+        condition = self.condition
+        
+        # Increment the corresponding stock field based on the condition
+        if condition:
+            if condition == 'acceptable':
+                self.stock_acceptable += 1
+            elif condition == 'good':
+                self.stock_good += 1
+            elif condition == 'excellent':
+                self.stock_excellent += 1
+
+        super().save(*args, **kwargs)
+
+
+class Laptop(Product):
 
     SIZES = [
         (11, 11),
@@ -39,127 +73,76 @@ class Laptop(models.Model):
         (8, 8),
         (10, 10),
         (12, 12)
-
     ]
 
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    brand = models.CharField(max_length=254)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(max_digits=8, null=True, blank=True, decimal_places=2)
-    model = models.CharField(max_length=254)
-    series = models.CharField(max_length=254)
-    color = models.CharField(max_length=254)
-    label = models.CharField(max_length=254)
-    cpu_brand = models.CharField(max_length=254)
-    cpu_name = models.CharField(max_length=254)
-    ssd = models.CharField(max_length=254, choices=CHOICES)
-    storage_size = models.CharField(max_length=254)
-    ram = models.CharField(max_length=254)
-    cpu_variant = models.CharField(max_length=254)
-    gpu = models.CharField(max_length=254)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    label = models.CharField(max_length=50)
+    cpu_brand = models.CharField(max_length=50)
+    cpu_name = models.CharField(max_length=50)
+    ssd = models.BooleanField(default=False)
+    storage_size = models.CharField(max_length=50)
+    ram = models.CharField(max_length=50)
+    cpu_variant = models.CharField(max_length=50)
+    gpu = models.CharField(max_length=50)
     cores = models.PositiveIntegerField(choices=CORES)
-    os = models.CharField(max_length=254)
-    usb = models.CharField(max_length=254)
-    hdmi = models.CharField(max_length=254)
-    touchscreen = models.CharField(max_length=254, choices=CHOICES)
+    os = models.CharField(max_length=50)
+    usb = models.CharField(max_length=50)
+    hdmi = models.CharField(max_length=50)
+    touchscreen = models.BooleanField(default=False)
     screen_size = models.PositiveIntegerField(choices=SIZES)
-    dimensions = models.CharField(max_length=254)
-    weight = models.CharField(max_length=254)
-    finger_print_sensor = models.CharField(max_length=254, choices=CHOICES)
-    keyboard = models.CharField(max_length=254)
-    backlit = models.CharField(max_length=254, choices=CHOICES)
-    image = models.ImageField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    dimensions = models.CharField(max_length=50)
+    weight = models.CharField(max_length=50)
+    finger_print_sensor = models.BooleanField(default=False)
+    keyboard = models.CharField(max_length=50)
+    backlit = models.BooleanField(default=False)
 
-    def set_sale_price(self, *args, **kwargs):
-        if self.sale:
-            self.sale_price = self.price * 0.95
-        else:
-            self.sale_price = None
 
-    def __str__(self):
-        return f'{self.brand} {self.series} {self.model}'
 
-class Phone(models.Model):
+class Phone(Product):
 
     OS = [
         ('Android', 'Android'),
         ('iOS', 'iOS')
     ]
     
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    brand = models.CharField(max_length=254)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(max_digits=8, null=True, blank=True, decimal_places=2)
-    series = models.CharField(max_length=254)
-    model = models.CharField(max_length=254)
-    sim = models.CharField(max_length=254)
-    cpu = models.CharField(max_length=254)
-    ram = models.CharField(max_length=254)
-    storage_size = models.CharField(max_length=254)
-    battery = models.CharField(max_length=254)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    sim = models.CharField(max_length=50)
+    cpu = models.CharField(max_length=50)
+    ram = models.CharField(max_length=50)
+    storage_size = models.CharField(max_length=50)
+    battery = models.CharField(max_length=50)
     screen_size = models.DecimalField(max_digits=4, decimal_places=2)
-    resolution = models.CharField(max_length=254)
-    camera_front = models.CharField(max_length=254)
-    camera_rear = models.CharField(max_length=254)
-    os = models.CharField(max_length=254, choices=OS)
-    color = models.CharField(max_length=254)
-    image = models.ImageField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    resolution = models.CharField(max_length=50)
+    camera_front = models.CharField(max_length=50)
+    camera_rear = models.CharField(max_length=50)
+    os = models.CharField(max_length=50, choices=OS)
 
-    def __str__(self):
-        return f'{self.brand} {self.series} {self.model}'
 
-class Smartwatch(models.Model):
 
-    CHOICES = [
-        ('yes', 'Yes'),
-        ('no', 'No'),
-    ]
+class Smartwatch(Product):
 
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    brand = models.CharField(max_length=254)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(max_digits=8, null=True, blank=True, decimal_places=2)
-    series = models.CharField(max_length=254)
-    model = models.CharField(max_length=254)
-    connectivity = models.CharField(max_length=254)
-    display = models.CharField(max_length=254)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    connectivity = models.CharField(max_length=50)
+    display = models.CharField(max_length=50)
     screen_size = models.DecimalField(max_digits=4, decimal_places=2)
-    resolution = models.CharField(max_length=254)
-    water_resistance = models.CharField(max_length=254)
-    heart_rate_monitor = models.CharField(max_length=254, choices=CHOICES)
-    gps = models.CharField(max_length=254, choices=CHOICES) 
-    os = models.CharField(max_length=254)
-    color = models.CharField(max_length=254)
-    image = models.ImageField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    resolution = models.CharField(max_length=50)
+    water_resistance = models.CharField(max_length=50)
+    heart_rate_monitor = models.BooleanField(default=False)
+    gps = models.BooleanField(default=False)
+    os = models.CharField(max_length=50)
 
-    def __str__(self):
-        return f'{self.brand} {self.series} {self.model}'
 
-class Console(models.Model):
 
-    BRAND = [
-        ('Microsoft', 'Microsoft'),
-        ('Sony', 'Sony'),
-        ('Nintendo', 'Nintendo'),
-    ]
+class Console(Product):
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    storage_size = models.CharField(max_length=50)
 
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
-    brand = models.CharField(max_length=254, choices=BRAND)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(max_digits=8, null=True, blank=True, decimal_places=2)
-    series = models.CharField(max_length=254)
-    model = models.CharField(max_length=254)
-    storage_size = models.CharField(max_length=254)
-    color = models.CharField(max_length=254)
-    image = models.ImageField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+# @receiver(post_save, sender=Product)
+# def update_stock(sender, instance, **kwargs):
+#     # Retrieve the selected condition for the product
+#     condition_name = instance.condition.name
 
-    def __str__(self):
-        return f'{self.brand} {self.series} {self.model}'
+#     # Update the stock for the selected condition
+#     condition = Condition.objects.get(name=condition_name)
+#     condition.stock += 1
+#     condition.save()
