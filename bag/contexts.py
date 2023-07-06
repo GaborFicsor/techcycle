fd6b10f925ea78ca63623bcbfa64422495750e65
GaborFicsor <ficsorgabor@gmail.com>
2023-07-06 19:54:37 +0000

    Update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,15 +1,26 @@
 from django.conf import settings
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0 
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, condition in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price
+        bag_items.append({
+            'item_id': item_id,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
     }
 
     return context
