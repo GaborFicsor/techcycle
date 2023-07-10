c300832e0c6c44649534c7a1dd2001c03e62c228
GaborFicsor <ficsorgabor@gmail.com>
2023-07-10 22:41:19 +0000

    Add buttons to product cards for editing and deleting

--- products/views.py
+++ products/views.py
@@ -236,9 +236,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -256,7 +256,7 @@ def edit_product(request, product_id):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messasges.success(request, 'Successfully updated product')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
@@ -272,3 +272,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """Delete a product from the store"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
