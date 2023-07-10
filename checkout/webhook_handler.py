918cfd282c568f6c41ead9cfa04196cd018e6bd5
GaborFicsor <ficsorgabor@gmail.com>
2023-07-10 21:07:56 +0000

    Add user_profile to webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -47,7 +47,7 @@ class StripeWH_Handler:
 
         profile = None
         username = intent.metadata.username
-        if username != 'AnonymiusUser':
+        if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
                 profile.default_phone_number = shipping_details.phone
@@ -94,6 +94,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
