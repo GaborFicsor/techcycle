12a357c8123af4a1086f661a2028b89aae0760f7
GaborFicsor <ficsorgabor@gmail.com>
2023-07-10 20:44:26 +0000

    Add functionality for logged in users to save their details when making an order

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 
 from .forms import OrderForm
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from .models import Order, OrderLineItem
 from bag.contexts import bag_contents
 
@@ -66,7 +68,7 @@ def checkout(request):
                     )
                     order_line_item.save()
 
-            request.session['save_info'] = 'save_info' in request.POST
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
@@ -97,19 +99,45 @@ def checkout(request):
     return render(request, template, context)
 
 def checkout_success(request, order_number):
-    """ Handle successful event of checkout """
+    """
+    Handle successful checkouts
+    """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county,
+                'default_postcode': order.postcode,
+                'default_country': order.country,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+                print(user_profile_form)
+                print(profile_data)
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
-    
+
     if 'bag' in request.session:
         del request.session['bag']
 
     template = 'checkout/checkout_success.html'
     context = {
-        'order': order
+        'order': order,
     }
 
     return render(request, template, context)
