ef6263f7d8d5082579ae780336f54e8127111cec
GaborFicsor <ficsorgabor@gmail.com>
2023-07-16 23:55:58 +0000

    Fix character typo

--- techcycle/settings.py
+++ techcycle/settings.py
@@ -182,7 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
 
     # cache control
-    AWS_S3_OBJECT_PARAMETERS + {
+    AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
