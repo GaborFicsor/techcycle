9ef3f8c86d59ab6597909cb735924c13d431facc
GaborFicsor <ficsorgabor@gmail.com>
2023-07-16 21:24:33 +0000

    Set workspace up for deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn techcycle.wsgi:application
--- techcycle/settings.py
+++ techcycle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-j)^v4)e#3-j#5s9fzmeek83n-5xe61d(u#_d6tlfvz$upe39zu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','8000-gaborficsor-techcycle-wgmppxamqda.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['localhost','8000-gaborficsor-techcycle-wgmppxamqda.ws-eu101.gitpod.io', 'techcycle.herokuapp.com']
 
 
 # Application definition
@@ -122,13 +123,17 @@ WSGI_APPLICATION = 'techcycle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
