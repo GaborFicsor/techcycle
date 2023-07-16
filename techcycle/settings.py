e456a1d98a8348425f0e99ad1ebea192a0af457c
GaborFicsor <ficsorgabor@gmail.com>
2023-07-16 23:14:40 +0000

    Add cache control

--- techcycle/settings.py
+++ techcycle/settings.py
@@ -180,6 +180,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+
+    # cache control
+    AWS_S3_OBJECT_PARAMETERS + {
+        'Expires': 'Thu 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'techcycle'
     AWS_S3_REGION_NAME = 'eu-north-1'
